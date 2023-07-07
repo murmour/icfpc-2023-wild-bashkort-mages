4d6d60d157030ab3b0fc622ff3dcb7b6eaedb6ce
Max Mouratov <me@murmour.me>
2023-07-07 23:29:47 +0900

    手

---
+++ 手.py
@@ -0,0 +1,82 @@
+
+import os
+import io
+import re
+import sys
+import subprocess
+import json
+import time
+import shutil
+
+
+api = 'https://api.icfpcontest.com'
+api_key = (
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiI2NDlkZTZhMjh'
+    'jNjg1MzEzZDFjNjBkNjEiLCJpYXQiOjE2ODgwNjk3OTQsImV4cCI6MTY5ODA'
+    '2OTc5NH0.svNmE8_E5X7tEQjnqNF9rtdlgOckN21pZJ85br6uFOw'
+)
+
+prob_count = 45
+
+
+def get_prob_request(id: int) -> dict:
+    res = subprocess.check_output(
+        ['curl',
+         '--compressed',
+         '-H', f'Authorization: Bearer {api_key}',
+         f'{api}/problem?problem_id={id}' ])
+    time.sleep(1)
+    res_js = json.loads(res.decode('utf-8'))
+    assert('Success' in res_js)
+    return json.loads(res_js['Success'])
+
+
+def get_prob_path(id: int) -> str:
+    path = f'問/{id}.problem'
+    assert(os.path.exists(path))
+    return path
+
+
+def download_and_save_problem(id: int):
+    print(f'Downloading problem {id}...')
+    js = get_prob_request(id)
+    fname = get_prob_path(id)
+    with io.open(fname, 'w') as f:
+        f.write(json.dumps(js))
+        print(f'Saved to {fname}')
+
+
+def all_prob_ids() -> list[int]:
+    return [ i+1 for i in range(prob_count) ]
+
+
+def read_prob(id: int) -> dict:
+    fname = get_prob_path(id)
+    with io.open(fname, 'r') as h:
+        return json.loads(h.read())
+
+
+def print_prob_stats(id: int) -> dict:
+    prob = read_prob(id)
+    print(f'{id}.problem:')
+    print(f'musician_count: {len(prob["musicians"])}')
+    print(f'instument_count: {len(set(prob["musicians"]))}')
+    print(f'attendee_count: {len(prob["attendees"])}')
+    print()
+
+
+if __name__ == '__main__':
+    cmd = sys.argv[1]
+
+    if cmd == 'get_probs':
+        for pid in all_prob_ids():
+            download_and_save_problem(pid)
+        exit(0)
+
+    if cmd == 'print_prob_stats':
+        for pid in all_prob_ids():
+            print_prob_stats(pid)
+        exit(0)
+
+    print(f'invalid command: {cmd}')
+    exit(1)
