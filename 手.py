b729c0fc18bc840f8716c71a5e56acc6aee84a7d
Max Mouratov <me@murmour.me>
2023-07-08 14:22:41 +0900

    手: Засылатор

--- 手.py
+++ 手.py
@@ -20,7 +20,15 @@ prob_count = 55
 
 
 def all_prob_ids() -> list[int]:
-    return [ i+1 for i in range(55) ]
+    return [ i+1 for i in range(prob_count) ]
+
+
+def get_prob_path(prob_id: int) -> str:
+    return f'問/{prob_id}.problem'
+
+
+def get_sol_dir_path(prob_id: int) -> str:
+    return f'答/{prob_id}'
 
 
 def get_prob_request(prob_id: int) -> dict:
@@ -34,29 +42,6 @@ def get_prob_request(prob_id: int) -> dict:
     res_js = json.loads(res.decode('utf-8'))
     assert('Success' in res_js)
     return json.loads(res_js['Success'])
-
-
-def update_username_request(username: str) -> None:
-    data = json.dumps({ 'username': username })
-    res = subprocess.check_output([
-        'curl',
-        '--compressed',
-        '-H', f'Authorization: Bearer {api_key}',
-        f'{api}/user/update_username',
-        '--json', data
-    ])
-    time.sleep(1)
-    print(res)
-    res_js = json.loads(res.decode('utf-8'))
-    assert('Success' in res_js)
-
-
-def get_prob_path(prob_id: int) -> str:
-    return f'問/{prob_id}.problem'
-
-
-def get_sol_dir_path(prob_id: int) -> str:
-    return f'答/{prob_id}'
 
 
 def download_and_save_problem(prob_id: int):
@@ -77,18 +62,83 @@ def get_prob(prob_id: int) -> dict:
         return json.loads(h.read())
 
 
+# Make a submission. Returns a submission ID.
+def send_sol_request(prob_id: int, sol_tag: str) -> str:
+    print(f'Sending {prob_id}.{sol_tag}...')
+    sol = get_sol(prob_id, sol_tag)
+    sub = { 'problem_id': prob_id, 'contents': json.dumps(sol) }
+    sub_path = 'submission.js'
+    with io.open(sub_path, 'w') as f:
+        f.write(json.dumps(sub))
+    res = subprocess.check_output([
+        'curl',
+        '--json', f'@{sub_path}',
+        '-H', f'Authorization: Bearer {api_key}',
+        f'{api}/submission',
+    ])
+    time.sleep(1)
+    res = res.decode('utf-8')
+    print(f'response: {res}')
+    return res
+
+
 def get_sol_tags(prob_id: int) -> list:
-    sols_dir = get_sol_dir_path(prob_id)
-    tags = [ f[:-9] for f in os.listdir(sols_dir) if f.endswith('.solution') ]
+    sol_dir = get_sol_dir_path(prob_id)
+    tags = [ f[:-9] for f in os.listdir(sol_dir) if f.endswith('.solution') ]
     tags.sort()
     return tags
 
 
 def get_sol(prob_id: int, tag: str) -> dict:
-    sols_dir = get_sol_dir_path(prob_id)
-    fname = f'{sols_dir}/{tag}.solution'
+    sol_dir = get_sol_dir_path(prob_id)
+    fname = f'{sol_dir}/{tag}.solution'
     with io.open(fname, 'r') as h:
         return json.loads(h.read())
+
+
+def get_score(prob_id: int, sol_tag: str) -> float | None:
+    prob_path = get_prob_path(prob_id)
+    sol_dir = get_sol_dir_path(prob_id)
+    sol_path = f'{sol_dir}/{sol_tag}.solution'
+    sys.stdout.flush()
+    try:
+        sol = subprocess.check_output([
+            '../solver/solver',
+            '-pp', prob_path,
+            '-score', sol_path,
+        ])
+        return float(sol.decode())
+    except subprocess.CalledProcessError as ex: # error code <> 0
+        print('-------- ERROR --------')
+        print(ex)
+        sys.stdout.flush()
+        return None
+
+
+def get_best_sol(prob_id) -> str | None:
+    best_score = None
+    best_tag = None
+    for tag in get_sol_tags(prob_id):
+        score = get_score(prob_id, tag)
+        if score is None:
+            continue
+        if best_score == None or score > best_score:
+            best_score = score
+            best_tag = tag
+    if best_score == None:
+        print(f'{prob_id}: -')
+        return None
+    print(f'{prob_id}: {best_tag} ({score})')
+    return best_tag
+
+
+def get_local_best() -> dict:
+    r = {}
+    for prob_id in all_prob_ids():
+        sol = get_best_sol(prob_id)
+        if sol is not None:
+            r[prob_id] = sol
+    return r
 
 
 def print_prob_stats(prob_id: int) -> dict:
@@ -157,34 +207,15 @@ def run_solver(solver_id: str, solver_args: str, prob_id: int) -> str | None:
         return None
 
 
-def get_score(prob_id: int, sol_tag: str) -> float | None:
-    prob_path = get_prob_path(prob_id)
-    sols_dir = get_sol_dir_path(prob_id)
-    sol_path = f'{sols_dir}/{sol_tag}.solution'
-    sys.stdout.flush()
-    try:
-        sol = subprocess.check_output([
-            '../solver/solver',
-            '-pp', prob_path,
-            '-score', sol_path,
-        ])
-        return float(sol.decode())
-    except subprocess.CalledProcessError as ex: # error code <> 0
-        print('-------- ERROR --------')
-        print(ex)
-        sys.stdout.flush()
-        return None
-
-
-def save_sol(solver_id: str, prob_id: int, sol_id: str, sol: str) -> None:
-    print(f'saving {solver_id}({prob_id}) as {sol_id}')
-    sols_dir = get_sol_dir_path(prob_id)
-    sol_path = f'{sols_dir}/{sol_id}.solution'
+def save_sol(solver_id: str, prob_id: int, sol_tag: str, sol: str) -> None:
+    print(f'saving {solver_id}({prob_id}) as {sol_tag}')
+    sol_dir = get_sol_dir_path(prob_id)
+    sol_path = f'{sol_dir}/{sol_tag}.solution'
     with io.open(sol_path, 'wb') as h:
         h.write(sol.encode())
 
 
-def solve(solver_id: str, solver_args: str, sol_id: str, prob_id: int) -> None:
+def solve(solver_id: str, solver_args: str, sol_tag: str, prob_id: int) -> None:
     sol = run_solver(solver_id, solver_args, prob_id)
     if sol is None:
         print(f'{solver_id}({prob_id}) -> ERROR')
@@ -192,10 +223,10 @@ def solve(solver_id: str, solver_args: str, sol_id: str, prob_id: int) -> None:
     sol_js = json.loads(sol)
     score = get_score(sol_js)
     print(f'{solver_id}({prob_id}) -> OK ({score})')
-    save_sol(solver_id, prob_id, sol_id, sol)
+    save_sol(solver_id, prob_id, sol_tag, sol)
 
 
-def parsolve(solver_id, solver_args, sol_id, start_id, end_id, job_count) -> None:
+def parsolve(solver_id, solver_args, sol_tag, start_id, end_id, job_count) -> None:
 
     def start_solving(prob_id: int) -> dict:
         cmd = get_solver_cmd(solver_id, solver_args, prob_id)
@@ -214,7 +245,7 @@ def parsolve(solver_id, solver_args, sol_id, start_id, end_id, job_count) -> Non
         sol = job['process'].communicate()[0].decode()
         score = get_score(sol)
         print(f'{solver_id}({prob_id}) -> OK ({score})')
-        save_sol(solver_id, prob_id, sol_id, sol)
+        save_sol(solver_id, prob_id, sol_tag, sol)
 
     queue = list(range(start_id, end_id+1))
     pool = [None] * job_count
@@ -240,6 +271,7 @@ def parsolve(solver_id, solver_args, sol_id, start_id, end_id, job_count) -> Non
                 pool[i] = None
                 finish_solving(job, retcode)
 
+
 def get_best_score(prob_id: int) -> float:
     sol_tags = get_sol_tags(prob_id)
     best = None
@@ -251,6 +283,7 @@ def get_best_score(prob_id: int) -> float:
             best_score = score
     return best_score
 
+
 def get_sorted_scores():
     data = []
     for pid in all_prob_ids():
@@ -258,6 +291,21 @@ def get_sorted_scores():
     data.sort(reverse=True)
     for score, pid in data:
         print('%12.0f %3d' % (score, pid))
+
+
+def update_username_request(username: str) -> None:
+    data = json.dumps({ 'username': username })
+    res = subprocess.check_output([
+        'curl',
+        '--compressed',
+        '-H', f'Authorization: Bearer {api_key}',
+        f'{api}/user/update_username',
+        '--json', data
+    ])
+    time.sleep(1)
+    print(res)
+    res_js = json.loads(res.decode('utf-8'))
+    assert('Success' in res_js)
 
 
 if __name__ == '__main__':
@@ -283,6 +331,12 @@ if __name__ == '__main__':
 
     if cmd == 'get_sorted_scores':
         get_sorted_scores()
+        exit(0)
+
+    if cmd == 'send_all_best':
+        best = get_local_best()
+        for prob_id, sol_tag in best.items():
+            send_sol_request(prob_id, sol_tag)
         exit(0)
 
     if cmd == 'update_username':
